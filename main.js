93784d8e81d8bc0fab5af7f3f5e8e2de4004a762
Douglas Gordon <douglastgordon@gmail.com>
2018-08-13 09:41:18 -0400

    add curry function

--- main.js
+++ main.js
@@ -9,3 +9,8 @@ const constant = value => identity(value)
 
 // apply function partially
 const partial = (fn, ...args) => (...laterArgs) => fn(...args, ...laterArgs)
+
+const curry = (fn, arity=fn.length) => (nextCurried = prevArgs => nextArg => {
+  const args = [...prevArgs, nextArg]
+  return args.length >= arity ? fn(...args) : nextCurried(args)
+})([])
